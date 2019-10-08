44e874e1358c9170cdc51521205916753fa638fe
陆鹏 <792961190@qq.com>
2019-10-08 14:43:22 +0800

    图片放大提示组件

---
+++ antdUI/src/pages/UserUI/FirsePage.js
@@ -0,0 +1,113 @@
+import React, { PureComponent } from 'react';
+import {
+    Card,
+    Table,
+    Divider,
+    Tag,
+} from 'antd';
+import TooptipImg from '@/components/TooltipImg';
+
+class FirsePage extends PureComponent {
+    render() {
+        const columns = [
+            {
+                title: 'Picture',
+                dataIndex: 'picture',
+                key: 'picture',
+                render: text => <TooptipImg
+                    placement="right"
+                    trigger="hover"
+                    contentImgStyle={{
+                    maxHeight: 350,
+                    maxWidth: 350,
+                    }}
+                    url={text}
+                    imgStyle={{ maxHeight: 100, maxWidth: 132, marginRight: '5px' }}
+                    alts="头像"
+                />,
+            },
+            {
+              title: 'Name',
+              dataIndex: 'name',
+              key: 'name',
+              render: text => <a>{text}</a>,
+            },
+            {
+              title: 'Age',
+              dataIndex: 'age',
+              key: 'age',
+            },
+            {
+              title: 'Address',
+              dataIndex: 'address',
+              key: 'address',
+            },
+            {
+              title: 'Tags',
+              key: 'tags',
+              dataIndex: 'tags',
+              render: tags => (
+                <span>
+                  {tags.map(tag => {
+                    let color = tag.length > 5 ? 'geekblue' : 'green';
+                    if (tag === 'loser') {
+                      color = 'volcano';
+                    }
+                    return (
+                      <Tag color={color} key={tag}>
+                        {tag.toUpperCase()}
+                      </Tag>
+                    );
+                  })}
+                </span>
+              ),
+            },
+            {
+              title: 'Action',
+              key: 'action',
+              render: (text, record) => (
+                <span>
+                  <a>Invite {record.name}</a>
+                  <Divider type="vertical" />
+                  <a>Delete</a>
+                </span>
+              ),
+            },
+          ];
+
+          const data = [
+            {
+              key: '1',
+              name: 'John Brown',
+              picture: 'http://img3.imgtn.bdimg.com/it/u=3414414030,1436910499&fm=11&gp=0.jpg',
+              age: 32,
+              address: 'New York No. 1 Lake Park',
+              tags: ['nice', 'developer'],
+            },
+            {
+              key: '2',
+              name: 'Jim Green',
+              picture: 'http://pic4.zhimg.com/50/v2-46c2eb18d011d3711617a2664844e5fd_hd.jpg',
+              age: 42,
+              address: 'London No. 1 Lake Park',
+              tags: ['loser'],
+            },
+            {
+              key: '3',
+              name: 'Joe Black',
+              picture: 'http://www.banyingle.com/shop/images_shop/24622/2012-01-12/69872012112106083970703.jpg',
+              age: 32,
+              address: 'Sidney No. 1 Lake Park',
+              tags: ['cool', 'teacher'],
+            },
+          ];
+
+        return (
+            <Card>
+               <Table columns={columns} dataSource={data} />
+            </Card>
+        )
+    }
+}
+
+export default FirsePage;
